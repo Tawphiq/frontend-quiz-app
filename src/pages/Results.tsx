14db1cf6cc651a0dfa6ba78db2efe08451d67e1c
tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-06-12 00:14:37 +0000

    "results component"

---
+++ src/pages/Results.tsx
@@ -0,0 +1,24 @@
+// src/pages/Results.tsx
+import React from 'react';
+
+interface ResultsProps {
+  score: number;
+  totalQuestions: number;
+  handlePlayAgain: () => void;
+}
+
+const Results: React.FC<ResultsProps> = ({ score, totalQuestions, handlePlayAgain }) => {
+
+
+  return (
+    <div className="text-center">
+      <h2 className="text-xl md:text-2xl font-bold mb-4">Quiz Completed</h2>
+      <p>Your score: {score} / {totalQuestions}</p>
+      <button onClick={handlePlayAgain} className="mt-4 p-2 bg-purple text-white rounded w-full md:w-auto">
+        Play Again
+      </button>
+    </div>
+  );
+};
+
+export default Results;
