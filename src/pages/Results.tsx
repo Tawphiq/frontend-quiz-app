da5058d123092694d3f8b5a2fba86fc5200dd48b
tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-06-23 21:50:54 +0000

    "deleted assets from the public folder"

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,5 +1,6 @@
 // src/pages/Results.tsx
 import React from 'react';
+import { useQuiz } from '../context/QuizContext';
 
 interface ResultsProps {
   score: number;
@@ -8,15 +9,25 @@ interface ResultsProps {
 }
 
 const Results: React.FC<ResultsProps> = ({ score, totalQuestions, handlePlayAgain }) => {
+  const { currentQuiz } = useQuiz();
 
 
   return (
     <div className="text-center">
-      <h2 className="text-xl md:text-2xl font-bold mb-4">Quiz Completed</h2>
-      <p>Your score: {score} out of {totalQuestions}</p>
+      <div>
+      <h2 className="text-xl md:text-2xl">Quiz Completed</h2>
+      <h1 className='mb-4'>You scored...</h1>
+      </div>
+      {currentQuiz && (<div>
+      <div className='flex justify-center gap-2'>
+      <img src={currentQuiz.icon} alt="" />
+      <span className='flex items-center'>{currentQuiz.title}</span>
+      </div>
+      <p>{score} out of {totalQuestions}</p>
       <button onClick={handlePlayAgain} className="mt-4 p-2 bg-purple text-white rounded w-full md:w-auto">
         Play Again
       </button>
+      </div>)}
     </div>
   );
 };
