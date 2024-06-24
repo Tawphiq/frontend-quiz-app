007271a054a51af63cc2b9d1a0a9cd8d9759d4ba
tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-06-24 00:38:06 +0000

    "padding for the results page"

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -13,21 +13,21 @@ const Results: React.FC<ResultsProps> = ({ score, totalQuestions, handlePlayAgai
 
 
   return (
-    <div className="p-5 lg:grid lg:grid-cols-2">
+    <div className="p-2 lg:p-0 lg:grid lg:grid-cols-2">
       <div>
-      <h2 className="font-thin text-4xl md:text-6xl lg:text-5xl">Quiz Completed</h2>
-      <h1 className='font-bold text-dark dark:text-white text-4xl md:text-6xl lg:text-5xl mb-8'>You scored...</h1>
+      <h2 className="font-thin text-dark dark:white text-4xl md:text-6xl lg:text-7xl">Quiz Completed</h2>
+      <h1 className='font-bold text-dark dark:text-white text-4xl md:text-6xl lg:text-7xl mb-8'>You scored...</h1>
       </div>
-      {currentQuiz && (<div className=''>
-      <div className='bg-white dark:bg-dark p-5 rounded-md'>
+      {currentQuiz && (<div className='lg:mx-14'>
+      <div className='bg-white dark:bg-dark p-5 rounded-lg shadow '>
       <div className='flex justify-center gap-2'>
       <img src={currentQuiz.icon} alt="" />
       <span className='grid items-center font-bold text-dark dark:text-white text-xl md:text-2xl'>{currentQuiz.title}</span>
       </div>
-      <p className='grid justify-center text-6xl text-dark dark:text-white my-4'>{score}</p>
+      <p className='grid justify-center text-6xl lg:text-9xl text-dark dark:text-white my-4 lg:my-8'>{score}</p>
       <p className='grid justify-center text-dark dark:text-white'>out of {totalQuestions}</p>
       </div>
-      <button onClick={handlePlayAgain} className="mt-4 p-2 bg-purple text-white rounded-md h-14 w-full">
+      <button onClick={handlePlayAgain} className="mt-4 p-2 bg-purple text-white rounded-md shadow h-14 w-full">
         Play Again
       </button>
       </div>)}
