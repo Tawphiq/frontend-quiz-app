28b2d536ace216199efe81b902fe4f13af4a9da2
tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-06-24 00:16:18 +0000

    "implemeted the mobile results page"

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -13,18 +13,21 @@ const Results: React.FC<ResultsProps> = ({ score, totalQuestions, handlePlayAgai
 
 
   return (
-    <div className="text-center">
+    <div className="p-5 lg:grid lg:grid-cols-2">
       <div>
-      <h2 className="text-xl md:text-2xl">Quiz Completed</h2>
-      <h1 className='mb-4'>You scored...</h1>
+      <h2 className="font-thin text-4xl md:text-6xl lg:text-5xl">Quiz Completed</h2>
+      <h1 className='font-bold text-dark dark:text-white text-4xl md:text-6xl lg:text-5xl mb-8'>You scored...</h1>
       </div>
-      {currentQuiz && (<div>
+      {currentQuiz && (<div className=''>
+      <div className='bg-white dark:bg-dark p-5 rounded-md'>
       <div className='flex justify-center gap-2'>
       <img src={currentQuiz.icon} alt="" />
-      <span className='flex items-center'>{currentQuiz.title}</span>
+      <span className='grid items-center font-bold text-dark dark:text-white text-xl md:text-2xl'>{currentQuiz.title}</span>
       </div>
-      <p>{score} out of {totalQuestions}</p>
-      <button onClick={handlePlayAgain} className="mt-4 p-2 bg-purple text-white rounded w-full md:w-auto">
+      <p className='grid justify-center text-6xl text-dark dark:text-white my-4'>{score}</p>
+      <p className='grid justify-center text-dark dark:text-white'>out of {totalQuestions}</p>
+      </div>
+      <button onClick={handlePlayAgain} className="mt-4 p-2 bg-purple text-white rounded-md h-14 w-full">
         Play Again
       </button>
       </div>)}
