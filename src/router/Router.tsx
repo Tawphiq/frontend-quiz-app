cc4c8664f8299888f4abcf2ff33a74daa8eb9fe9
tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-06-25 15:33:17 +0000

    "removed the Results component from the router"

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Quiz from '../pages/Quiz';
-import Result from '../pages/Results';
 import Header from '../components/Header';
 
 const AppRouter: React.FC = () => {
@@ -13,7 +12,6 @@ const AppRouter: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quiz/:subject" element={<Quiz />} />
-        <Route path="/result" element={<Result />} />
       </Routes>
     </Router>
   );
