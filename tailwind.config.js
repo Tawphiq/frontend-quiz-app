e4d8d33a413d68175e1d57152b6ca5840a7ec0b1
tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-06-28 17:47:17 +0000

    "commented the color configurations out to experiment"

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,15 +16,15 @@ export default {
         'desktop-light': "url('/src/assets/images/pattern-background-desktop-light.svg')",
         'desktop-dark': "url('/src/assets/images/pattern-background-desktop-dark.svg')",
       },
-      colors: {
-        lightbg: "#F4F6FA",
-        darkbg: "#313E51",
-        dark: "#3B4D66",
-        darkthin: "#626C7F",
-        thinlight: "#ABC1E1",
-        purple:"#A729F5",
+      // colors: {
+      //   lightbg: "#F4F6FA",
+      //   darkbg: "#313E51",
+      //   dark: "#3B4D66",
+      //   darkthin: "#626C7F",
+      //   thinlight: "#ABC1E1",
+      //   purple:"#A729F5",
 
-      },
+      // },
     },
     fontFamily: {
       rubik:["Rubik", "Nunito"]
