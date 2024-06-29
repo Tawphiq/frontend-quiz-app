bb30182fc42f54e6de39396f9325652044afe391
tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-06-29 01:50:50 +0000

    "added the jit mode"

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     './dist/index.html',
   ],
+  mode:'jit',
   theme: {
     extend: {
       backgroundImage: {
@@ -16,15 +17,6 @@ export default {
         'desktop-light': "url('/src/assets/images/pattern-background-desktop-light.svg')",
         'desktop-dark': "url('/src/assets/images/pattern-background-desktop-dark.svg')",
       },
-      // colors: {
-      //   lightbg: "#F4F6FA",
-      //   darkbg: "#313E51",
-      //   dark: "#3B4D66",
-      //   darkthin: "#626C7F",
-      //   thinlight: "#ABC1E1",
-      //   purple:"#A729F5",
-
-      // },
     },
     fontFamily: {
       rubik:["Rubik", "Nunito"]
